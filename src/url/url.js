50d4da57dc72a110a59580c8d8b2aba58fa1c422
cuong.le <cuong.le@levincigroup.com>
2020-05-18 14:45:34 +0700

    add staging

--- src/url/url.js
+++ src/url/url.js
@@ -1,8 +1,8 @@
-// //! TEST
-const URL = "https://api2.levincidemo.com/api";
-export const SignalURL = "https://api2.levincidemo.com";
-export const upFileUrl =
-  "https://api2.levincidemo.com/api/file?category=service";
+// // //! TEST
+// const URL = "https://api2.levincidemo.com/api";
+// export const SignalURL = "https://api2.levincidemo.com";
+// export const upFileUrl =
+//   "https://api2.levincidemo.com/api/file?category=service";
 //! PRODUCT
 // const URL = "https://hp-admin-prod-westus.azurewebsites.net/api";
 // export const SignalURL = "https://hp-admin-prod-westus.azurewebsites.net";
@@ -13,4 +13,12 @@ export const upFileUrl =
 // export const SignalURL = "https://admin.harmonypayment.com";
 // export const upFileUrl =
 //   "https://admin.harmonypayment.com/api/file?category=product";
+// export default URL;
+
+// STAGING
+const URL = "https://staging.harmonypayment.com/api";
+export const SignalURL = "https://staging.harmonypayment.com";
+export const upFileUrl =
+  "https://staging.harmonypayment.com/api/file?category=service";
+//! PRODUCT
 export default URL;
