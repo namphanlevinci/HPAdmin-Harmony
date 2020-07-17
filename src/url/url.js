a632ed4b5348cfc71e655b6fa738b172a1728397
Cuong Le <cuongle@Cuongs-iMac.local>
2020-07-17 10:29:26 +0700

    merge staging

--- src/url/url.js
+++ src/url/url.js
@@ -4,32 +4,19 @@
 //   "https://dev.harmonypayment.com/api/file?category=service";
 
 // PRODUCT ADMIN
-<<<<<<< HEAD
-const URL = "https://admin.harmonypayment.com/api";
-export const upFileUrl =
-  "https://admin.harmonypayment.com/api/file?category=product";
+// const URL = "https://admin.harmonypayment.com/api";
+// export const upFileUrl =
+//   "https://admin.harmonypayment.com/api/file?category=product";
 
 // STAGING
 // const URL = "https://staging.harmonypayment.com/api";
 // export const upFileUrl =
 //   "https://staging.harmonypayment.com/api/file?category=service";
-=======
-// const URL = "https://admin.harmonypayment.com/api";
-// export const config.upFile =
-//   "https://admin.harmonypayment.com/api/file?category=product";
-
-// STAGING
-// const URL = "https://admin.stage.harmonypayment.com/api";
-// export const config.upFile =
-//   "https://admin.stage.harmonypayment.com/api/file?category=service";
->>>>>>> 71bac0a688b5fd1555ea5156718b5c050d8847e0
 
 const prod = {
   url: {
-    // URL: "https://admin.harmonypayment.com/api",
-    // upFile: "https://admin.harmonypayment.com/api/file?category=product",
-    URL: "https://admin.stage.harmonypayment.com/api",
-    upFile: "https://admin.stage.harmonypayment.com/apifile?category=service",
+    URL: "https://admin.harmonypayment.com/api",
+    upFile: "https://admin.harmonypayment.com/api/file?category=product",
   },
 };
 
@@ -37,10 +24,8 @@ const dev = {
   url: {
     // URL: "https://dev.harmonypayment.com/api",
     // upFile: "https://dev.harmonypayment.com/api/file?category=service",
-    URL: "https://admin.stage.harmonypayment.com/api",
-    upFile: "https://admin.stage.harmonypayment.com/apifile?category=service",
-    // URL: "https://admin.harmonypayment.com/api",
-    // upFile: "https://admin.harmonypayment.com/api/file?category=product",
+    URL: "https://admin.harmonypayment.com/api",
+    upFile: "https://admin.harmonypayment.com/api/file?category=product",
   },
 };
 
