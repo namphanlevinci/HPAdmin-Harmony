4a1e440c283dbcbb2ab9088e7dd51b03063f05f1
cuong.le <cuong.le@levincigroup.com>
2020-07-20 14:59:36 +0700

    fix loi permission

--- src/util/checkPermission.js
+++ src/util/checkPermission.js
@@ -11,27 +11,27 @@ function CheckPermissions(name, permissionName) {
   //   (page) => page.modulePage === name
   // );
 
-  const filterPermissionByPage = checkPermission
-    ?.filter((obj) => obj.modulePage === name)
-    .map((filteredObj) => filteredObj.actions);
+  // const filterPermissionByPage = checkPermission
+  //   ?.filter((obj) => obj.modulePage === name)
+  //   .map((filteredObj) => filteredObj.actions);
 
   // const coverToObject = filterPermissionByPage[0].reduce(
   //   (obj, item) => ({ ...obj, [item.action]: item.roleIsActive }),
   //   {}
   // );
 
-  const getPermissionByName = filterPermissionByPage[0]
-    .filter(({ action }) => action === permissionName)
-    .reduce((obj, item) => item.roleIsActive, {});
+  // const getPermissionByName = filterPermissionByPage[0]
+  //   .filter(({ action }) => action === permissionName)
+  //   .reduce((obj, item) => item.roleIsActive, {});
 
   // const checkPermissionByRole = coverToObject.filter(
   //   (role) => role === permissionName
   // );
 
-  console.log("filterPermissionByPage", filterPermissionByPage);
-  console.log("getPermissionByName", getPermissionByName);
+  // console.log("filterPermissionByPage", filterPermissionByPage);
+  // console.log("getPermissionByName", getPermissionByName);
 
-  return getPermissionByName;
+  // return getPermissionByName;
 }
 
 export default CheckPermissions;
