629a30d3b2e101b1cc750e57f5ca96b8c10994af
Nam <nam.phan@levincigroup.com>
2021-05-10 15:28:20 +0700

    fix add staff

--- src/app/routes/Merchants/MerchantList/Profile/Staff/AddStaff/index.js
+++ src/app/routes/Merchants/MerchantList/Profile/Staff/AddStaff/index.js
@@ -115,27 +115,23 @@ class AddStaff extends Component {
     }
   };
 
-  _submitForm = (values, actions) => {
+  submitForm = (values, actions) => {
     const merchantId = this.props.MerchantProfile.merchantId;
     const { activeStep } = this.state;
     const path = "/app/merchants/profile/staff";
     const payload = { ...values, merchantId, path };
 
-    this.props.AddMerchantStaffById(payload);
-
+    this.props.addStaff(payload);
     actions.setSubmitting(false);
-
-    this.setState({
-      activeStep: activeStep + 1,
-    });
+    this.setState({ activeStep: activeStep + 1 });
   };
 
-  _handleSubmit = (values, actions) => {
+  handleSubmit = (values, actions) => {
     const { activeStep } = this.state;
     const steps = this.getSteps();
     const isLastStep = activeStep === steps.length - 1;
     if (isLastStep) {
-      this._submitForm(values, actions);
+      this.submitForm(values, actions);
     } else {
       this.setState({
         activeStep: activeStep + 1,
@@ -185,9 +181,8 @@ class AddStaff extends Component {
                 return (
                   <Step
                     key={label}
-                    className={`horizontal-stepper ${
-                      index === activeStep ? "active" : ""
-                    }`}
+                    className={`horizontal-stepper ${index === activeStep ? "active" : ""
+                      }`}
                   >
                     <StepLabel className="stepperlabel">{label}</StepLabel>
                   </Step>
@@ -200,69 +195,69 @@ class AddStaff extends Component {
                   <CircularProgress size={50} />
                 </div>
               ) : (
-                <div>
-                  <Formik
-                    initialValues={formInitialValues}
-                    validationSchema={currentValidationSchema}
-                    onSubmit={this._handleSubmit}
-                  >
-                    {({
-                      values,
-                      isSubmitting,
-                      handleChange,
-                      setFieldValue,
-                    }) => (
-                      <Form noValidate>
-                        {this.getStepContent(
-                          activeStep,
-                          values,
-                          handleChange,
-                          setFieldValue
-                        )}
-                        <div
-                          style={{
-                            paddingTop: "20px",
-                            display: "flex",
-                            justifyContent: "space-between",
-                          }}
-                        >
-                          <span>
-                            <Button
-                              disabled={activeStep === 0}
-                              onClick={this.handleBack}
-                              className="btn btn-red"
-                            >
-                              Back
+                  <div>
+                    <Formik
+                      initialValues={formInitialValues}
+                      validationSchema={currentValidationSchema}
+                      onSubmit={this.handleSubmit}
+                    >
+                      {({
+                        values,
+                        isSubmitting,
+                        handleChange,
+                        setFieldValue,
+                      }) => (
+                        <Form noValidate>
+                          {this.getStepContent(
+                            activeStep,
+                            values,
+                            handleChange,
+                            setFieldValue
+                          )}
+                          <div
+                            style={{
+                              paddingTop: "20px",
+                              display: "flex",
+                              justifyContent: "space-between",
+                            }}
+                          >
+                            <span>
+                              <Button
+                                disabled={activeStep === 0}
+                                onClick={this.handleBack}
+                                className="btn btn-red"
+                              >
+                                Back
                             </Button>
-                            <Button
-                              variant="contained"
-                              color="primary"
-                              type="submit"
-                              className="btn btn-green"
-                            >
-                              {activeStep === steps.length - 1
-                                ? "Finish"
-                                : "Next"}
+                              <Button
+                                variant="contained"
+                                color="primary"
+                                type="submit"
+                                className="btn btn-green"
+                              >
+                                {activeStep === steps.length - 1
+                                  ? "Finish"
+                                  : "Next"}
+                              </Button>
+                            </span>
+                            <span>
+                              <Button
+                                onClick={() =>
+                                  this.props.history.push(
+                                    "/app/merchants/profile/staff"
+                                  )
+                                }
+                                className="btn btn-red"
+                              >
+                                Cancel
                             </Button>
-                          </span>
-                          <span>
-                            <Button
-                              onClick={() =>
-                                this.props.history.push(
-                                  "/app/merchants/profile/staff"
-                                )
-                              }
-                              className="btn btn-red"
-                            >
-                              Cancel
-                            </Button>
-                          </span>
-                        </div>{" "}
-                      </Form>
-                    )}
-                  </Formik>
-                </div>
-              )}
+                            </span>
+                          </div>{" "}
+                        </Form>
+                      )}
+                    </Formik>
+                  </div>
+                )}
             </div>
           </div>
         </div>
@@ -275,7 +270,7 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  AddMerchantStaffById: (payload) => {
+  addStaff: (payload) => {
     dispatch(AddMerchantStaffById(payload));
   },
   warningNotify: (message) => {
